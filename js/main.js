97c3acba3f8c3355a76168daed1a6aa71eb66804
Simone Giusti <info@simonegiusti.it>
2022-07-26 13:08:21 +0200

    live del mattino

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+
+// condizione da verificare ? valore di return se vero : valore di return se falso;
+
+
+let numeroDaControllare = 4;
+
+let pippo;
+
+if (numeroDaControllare % 2 == 0) {         //prima parte
+    pippo = 'Pari';                         //seconda parte
+} else {
+    pippo = 'Dispari';                      //terza parte
+}
+
+
+// prima parte                               seconda parte           terza parte
+let pluto = (numeroDaControllare % 2 == 0) ? 'Pari'        :         'Dispari';
+
+
+
+
+
+
+
+//crea attributo in un tag
+document.getElementById('pippo').setAttribute('data-prova', 'ambarabacicciccocco');
+
+//recupera attributo in un tag
+console.log(document.getElementById('pippo').getAttribute('data-numerodiuscite'));
+
+
+
+
+
+
+
+
+
+
+
+
+
